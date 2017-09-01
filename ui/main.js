8039d8880b3da72a60ebd4efd1e29e3c1a456ea6
Rikhu7 <30743140+Rikhu7@users.noreply.github.com>
2017-09-01 18:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //change the text has been has been skipped
 //move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '300px';
 };
