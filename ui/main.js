9e334815b589a90623035a5d182bc49f25175e94
Rikhu7 <30743140+Rikhu7@users.noreply.github.com>
2017-09-02 16:55:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,5 +35,5 @@ submit.onclick = function() {
       list += '<li>' + names[i] + '</li>';
   }
   var ul = document.getElementById('namelist');
-  ul.innerHTML=namelist;
+  ul.innerHTML = list;
 };
